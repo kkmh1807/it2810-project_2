2d229a144b488f960397167813a58a09eec8288d
Jakob Keller <jekeller@stud.ntnu.no>
2022-09-13 14:34:52 +0200

    Fetch data form GitLab API

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Commits from './components/Commits';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>GitLab</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Commits />
     </div>
   );
 }
---
+++ src/components/Commits.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+interface Commit {
+  id: string;
+  short_id: string;
+  title: string;
+  author_name: string;
+  author_email: string;
+  authored_date: string;
+  committer_name: string;
+  committer_email: string;
+  committed_date: string;
+  created_at: string;
+  message: string;
+  parent_ids: string[];
+  web_url: string;
+}
+
+function Commits() {
+  const id = encodeURIComponent('it2810-h22/Team-37/project_2');
+  const [commits, setCommits] = useState<Commit[] | undefined>();
+  const [apiKey, setApiKey] = useState('');
+
+  function fetchCommits() {
+    //Ikke la den fetche etter at den alledrede er fetcha
+    fetch(`https://gitlab.stud.idi.ntnu.no/api/v4/projects/${id}/repository/commits`, {
+      headers: {
+        Authorization: `Bearer ${apiKey} `
+      }
+    }).then(async (response) => {
+      if (response.ok) {
+        const data: Commit[] = await response.json();
+        setCommits(data);
+      }
+    });
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setApiKey(e.target.value)} />
+      <button onClick={fetchCommits}>Start Browsing</button>
+      {commits &&
+        commits.map((commit, i) => (
+          <div key={i}>
+            <h1>{commit.author_name}</h1>
+            <p>{commit.committer_email}</p>
+            <p>{commit.title}</p>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default Commits;
