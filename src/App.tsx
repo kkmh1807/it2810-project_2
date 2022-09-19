89c0810fa3b8fb00c58a026f480ed7cd0e5495c2
Kine Kai Moe Holthe <kkholthe@stud.ntnu.no>
2022-09-19 11:31:56 +0200

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,24 @@ import Commits from './components/Commits';
 import MergeRequests from './components/MergeRequests';
 import Issues from './components/Issues';
 import Overview from './components/Overview';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ApiContextProvider>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/overview" element={<Overview />}></Route>
-          <Route path="/commits" element={<Commits />}></Route>
-          <Route path="/issues" element={<Issues />}></Route>
-          <Route path="/mergerequests" element={<MergeRequests />}></Route>
-        </Routes>
-      </ApiContextProvider>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <ApiContextProvider>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/overview" element={<Overview />}></Route>
+            <Route path="/commits" element={<Commits />}></Route>
+            <Route path="/issues" element={<Issues />}></Route>
+            <Route path="/mergerequests" element={<MergeRequests />}></Route>
+          </Routes>
+        </ApiContextProvider>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
