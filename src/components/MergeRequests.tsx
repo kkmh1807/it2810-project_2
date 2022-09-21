51835c842bf9aae9bc8326e993ca77f0d6c43fbe
Jakob Keller <jekeller@stud.ntnu.no>
2022-09-21 22:48:51 +0200

    styled MR page

--- src/components/MergeRequests.tsx
+++ src/components/MergeRequests.tsx
@@ -1,24 +1,32 @@
 import React, { useEffect } from 'react';
 import { MR } from '../types/models';
 import useGitlabData from '../hooks/useGitlabData';
+import '../styles/MergeRequest.css';
+import { useApiContext } from '../context/ApiContext';
 
 function MergeRequests() {
   const { data, fetchData } = useGitlabData<MR[]>('/merge_requests');
-
+  const endpoint = '/merge_requests/';
   useEffect(() => {
     fetchData();
   }, []);
 
+  const LinkData = useApiContext();
+
+  function urlToGitlab(endpoint: string, commitId: string) {
+    return `${LinkData.url}/${decodeURIComponent(LinkData.repo)}/-${endpoint}${commitId}`;
+  }
   return (
-    <div>
+    <div className="Container">
       {data &&
         data.map((mr, i) => (
-          <div key={i}>
-            <h1>{mr.title}</h1>
-            <p>{mr.source_branch}</p>
-            <p>{mr.author?.name}</p>
-            <p>{mr.reviewer?.name}</p>
-          </div>
+          <a className="mrContainer" key={i} href={urlToGitlab(endpoint, mr.iid.toString())} rel="noreferrer" target="_blank">
+            <div className="mr">
+              <h1>{mr.title}</h1>
+              <p>{mr.source_branch}</p>
+              <p id="author">{mr.author?.name}</p>
+            </div>
+          </a>
         ))}
     </div>
   );
