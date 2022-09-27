1dec366fabda592cdd0edc3059d3e4d1e02af128
Simen Sagholen Førrisdal <simensfo@stud.ntnu.no>
2022-09-27 14:17:18 +0200

    add random color picker

---
+++ src/utils/utils.ts
@@ -0,0 +1,17 @@
+export function generateColor(num: number) {
+  const newColor = () => Math.floor(Math.random() * 16777215).toString(16);
+
+  if (num > 1) {
+    const colorArray = [];
+    for (let i = 0; i < num; i++) {
+      let color = '#' + newColor();
+      while (colorArray.indexOf(color) > -1) {
+        color = newColor();
+      }
+      colorArray.push(color);
+    }
+    return colorArray;
+  } else {
+    return '#' + newColor();
+  }
+}
