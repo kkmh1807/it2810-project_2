27281c5d9d7b9c8abb1fffa4b136d288c6ed8ccf
Simen Sagholen Førrisdal <simensfo@stud.ntnu.no>
2022-09-30 14:05:18 +0200

    remove comments

--- src/components/Commits.tsx
+++ src/components/Commits.tsx
@@ -48,17 +48,6 @@ function Commits() {
         ) : (
           <div>No commits found on this branch</div>
         )}
-        {/* {data &&
-          data.map((commit, i) => (
-            <a className="card-link" href={urlToGitlab(linkData, urlEndpoint, commit.short_id)} key={i} rel="noreferrer" target="_blank">
-              <div className="commit-card">
-                <p className="auth-name">{commit.author_name}</p>
-                <p className="title">{commit.title}</p>
-                <p className="short-id">{commit.short_id}</p>
-                <p className="commit-date">{commit.committed_date.substring(0, 10)}</p>
-              </div>
-            </a>
-          ))} */}
       </div>
     </>
   );
