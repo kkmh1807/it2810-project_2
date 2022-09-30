4735463e1841cbbbe6fe008cdae8b16355041746
Simen Sagholen Førrisdal <simensfo@stud.ntnu.no>
2022-09-30 12:50:07 +0200

    move pie chart to own component for better error/loading states

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,27 +1,9 @@
-import useGitlabData from '../hooks/useGitlabData';
-import { Commit } from '../types/models';
 import '../styles/Overview.css';
-import { Pie } from 'react-chartjs-2';
 import 'chart.js/auto';
-import { generateColor } from '../utils/utils';
 import BarChart from './BarChart';
-
-const numberOfCommits = 500;
+import PieChart from './PieChart';
 
 const Overview = () => {
-  const { data } = useGitlabData<Commit[]>(`/repository/commits?all=true&per_page=${numberOfCommits}`);
-
-  if (!data) return <div>Oops, no data</div>;
-
-  const chartData = data.reduce((data, commit) => {
-    if (data[commit.author_name]) {
-      data[commit.author_name]++;
-    } else {
-      data[commit.author_name] = 1;
-    }
-    return data;
-  }, {} as Record<Commit['author_name'], number>);
-
   return (
     <div className="overview-wrapper">
       <div>
@@ -29,20 +11,7 @@ const Overview = () => {
         doloribus qui ab facere beatae, soluta, voluptates repellat laboriosam laudantium, fugit delectus veritatis aliquam vitae molestiae
         accusamus ex alias sit. Ratione ipsum quibusdam, qui ducimus iste dolorem animi autem aut cumque laudantium laboriosam!
       </div>
-      <div className="pie-chart-wrapper">
-        <p>Commits statistics for the last {numberOfCommits} commits in the repository</p>
-        <Pie
-          data={{
-            labels: Object.keys(chartData),
-            datasets: [
-              {
-                backgroundColor: generateColor(Object.keys(chartData).length),
-                data: Object.values(chartData)
-              }
-            ]
-          }}
-        />
-      </div>
+      <PieChart />
       <BarChart />
     </div>
   );
---
+++ src/components/PieChart.tsx
@@ -0,0 +1,44 @@
+import useGitlabData from '../hooks/useGitlabData';
+import { Commit } from '../types/models';
+import { Pie } from 'react-chartjs-2';
+import { generateColor } from '../utils/utils';
+
+const numberOfCommits = 200;
+
+const PieChart = () => {
+  const { data, isLoading, isError } = useGitlabData<Commit[]>(`/repository/commits?all=true&per_page=${numberOfCommits}`);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError) return <div>Something went wrong</div>;
+
+  if (!data?.length) return <div>No commits were found for this repository</div>;
+
+  const chartData = data.reduce((data, commit) => {
+    if (data[commit.author_name]) {
+      data[commit.author_name]++;
+    } else {
+      data[commit.author_name] = 1;
+    }
+    return data;
+  }, {} as Record<Commit['author_name'], number>);
+
+  return (
+    <div className="pie-chart-wrapper">
+      <p>Commits statistics for the last {numberOfCommits} commits in the repository</p>
+      <Pie
+        data={{
+          labels: Object.keys(chartData),
+          datasets: [
+            {
+              backgroundColor: generateColor(Object.keys(chartData).length),
+              data: Object.values(chartData)
+            }
+          ]
+        }}
+      />
+    </div>
+  );
+};
+
+export default PieChart;
