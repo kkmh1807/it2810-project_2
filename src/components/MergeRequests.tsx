334b8f14b5a15bd29161a22b2a2d641bcd0608ea
Simen Sagholen Førrisdal <simensfo@stud.ntnu.no>
2022-09-30 13:02:32 +0200

    loading and error fallback for merge requests page

--- src/components/MergeRequests.tsx
+++ src/components/MergeRequests.tsx
@@ -4,23 +4,32 @@ import '../styles/MergeRequest.css';
 import { urlToGitlab } from '../helper/Utils';
 import { useApiContext } from '../context/ApiContext';
 
+const endpoint = '/merge_requests/';
+
 function MergeRequests() {
-  const { data } = useGitlabData<MR[]>('/merge_requests');
-  const endpoint = '/merge_requests/';
-  const Linkdata = useApiContext();
+  const linkdata = useApiContext();
+
+  const { data, isLoading, isError } = useGitlabData<MR[]>(endpoint);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError) return <div>Something went wrong</div>;
 
   return (
     <div className="Container">
-      {data &&
+      {data?.length ? (
         data.map((mr, i) => (
-          <a className="mrContainer" key={i} href={urlToGitlab(Linkdata, endpoint, mr.iid.toString())} rel="noreferrer" target="_blank">
+          <a className="mrContainer" key={i} href={urlToGitlab(linkdata, endpoint, mr.iid.toString())} rel="noreferrer" target="_blank">
             <div className="mr">
               <h1>{mr.title}</h1>
               <p>{mr.source_branch}</p>
               <p id="author">{mr.author?.name}</p>
             </div>
           </a>
-        ))}
+        ))
+      ) : (
+        <div>No merge requests were found for this repository</div>
+      )}
     </div>
   );
 }
